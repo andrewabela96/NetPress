a0227da97af02a821960b483fb7909fa851f58b4
jamesborg <jamesborg2012@gmail.com>
2016-05-31 13:45:33 +0200

    Full Text Search

--- NetPressBlog/Controllers/HomeController.cs
+++ NetPressBlog/Controllers/HomeController.cs
@@ -10,10 +10,20 @@ namespace NetPressBlog.Controllers
 {
     public class HomeController : Controller
     {
+        private List<IQueryable> searchList = new List<IQueryable>();
         private NetPressDBEntity1 db = new NetPressDBEntity1();
 
-        public ActionResult Index()
+        public ActionResult Index(string search)
         {
+
+            if(!String.IsNullOrEmpty(search))
+            {
+                var posts = db.BlogInfoes.Where(p => p.Title.Contains(search) || (p.AspNetUser.UserName.Contains(search)) || p.Category.Type.Contains(search));
+                //searchList.Add(posts);
+
+
+                return View(posts);
+            }
           
             var blogs = db.BlogInfoes.OrderByDescending(b => b.DateCreated);
             var blogPub = blogs.Where(b => b.Status == 1);
